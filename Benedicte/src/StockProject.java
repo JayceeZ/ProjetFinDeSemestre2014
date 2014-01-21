6bb2629627a9f75e9c404ece7e7a862b89a941a2
GuillaumeRahbari <guillaume.rahbari@gmail.com>
2014-01-21 10:10:51 +0100

    import enlevés

--- Benedicte/src/StockProject.java
+++ Benedicte/src/StockProject.java
@@ -1,13 +1,7 @@
 
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.*;
-
 import controller.StockProjectController;
 
-import model.Appareil;
-import model.DataXML;
 import model.Emprunt;
 import model.Emprunteur;
 import model.Enseignant;
