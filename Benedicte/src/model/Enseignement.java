9ce238b67aef0fdb6928c2275ae9e1301e67ed12
GuillaumeRahbari <guillaume.rahbari@gmail.com>
2014-01-23 16:37:31 +0100

    javadoc enseignement

--- Benedicte/src/model/Enseignement.java
+++ Benedicte/src/model/Enseignement.java
@@ -9,16 +9,30 @@ package model;
 public enum Enseignement {
     SI("SI"), ELEC("ELEC"), GE("GE"), GB("GB"), MAM("MAM"), BAT("BAT");
     
+    // Le nom de l'enseignement
     private String nom;
+    
+    /**
+     * Le constructeur
+     * @param n
+     */
     private Enseignement(String n) {
     	this.nom = n;
     }
     
-    
+    /**
+     * Permet de renvoyer le nom correspondant a l'enseignement.
+     * @return Un String indiquant le nom correspondant a l'enseignement.
+     */
     public String toString() {
     	return nom;
     }
     
+    /**
+     * Permet de verifier que la chaine de caractere entree correspond bien a un enseignement.
+     * @param s La chaine de caractere qui doit etre verifiee.
+     * @return Un boolean indiquant si la chaine de caractere est un enseignement ou pas.
+     */
     public static boolean verifieEnum(String s) {
     	if(getEnum(s) != null) {
     		return true;
@@ -26,6 +40,11 @@ public enum Enseignement {
     	return false;
     }
     
+    /**
+     * Permet de renvoyer l'enseignement correspondant a la chaine de caractere entree.
+     * @param s La chaine de caractere correspondant a l'enseignement desire.
+     * @return Un Enseignement correspondant a la chaine de caractere entree.
+     */
     public static Enseignement getEnum(String s) {
     	for (Enseignement e : values()) {
     		if (e.toString().equals(s)) {
